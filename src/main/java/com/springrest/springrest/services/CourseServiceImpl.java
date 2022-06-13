6b643e5f5e3c8a51a67acb11beba737f7843fd75
Neeraj Kumar Ahirwar <2102899>
2022-06-13 14:40:30 +0530

    first commit

---
+++ src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,68 @@
+package com.springrest.springrest.services;
+
+import com.springrest.springrest.dao.CourseDao;
+import com.springrest.springrest.entities.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Service
+public class CourseServiceImpl implements CourseService{
+   @Autowired
+    private CourseDao courseDao;
+//    List<Course> list;
+    public CourseServiceImpl() {
+//        list = new ArrayList<>();
+//        list.add(new Course(125, "Java core course", "This course contain basic java"));
+//        list.add(new Course(123, "Spring rest", "This contain basic of spring rest"));
+    }
+
+    @Override
+    public List<Course> getCourse() {
+        return courseDao.findAll();
+    }
+
+    @Override
+    public Course getCourse(long courseId) {
+//        Course c = null;
+//        for(Course course : list){
+//            if(course.getId() == courseId){
+//                c = course;
+//                break;
+//            }
+//        }
+        return courseDao.getOne(courseId);
+    }
+
+    @Override
+    public Course addCourse(Course course) {
+//        list.add(course);
+        courseDao.save(course);
+        return course;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+//        list.forEach(e -> {
+//            if(e.getId() == course.getId()){
+//                e.setTitle(course.getTitle());
+//                e.setDescription(course.getDescription());
+//            }
+//        });
+        courseDao.save(course);
+        return course;
+    }
+
+    @Override
+    public void deleteCourse(long parseLong) {
+//        list = this.list.stream().filter(e->e.getId() != parseLong).collect(Collectors.toList());
+        Course entity = courseDao.getOne(parseLong);
+        courseDao.delete(entity);
+    }
+
+
+}
